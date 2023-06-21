24ee8bb4759bee1c54015726f39b9d2412f31cea
AkSam <sfnsi@gmail.com>
2023-06-22 00:04:43 +0500

    js first homework

---
+++ script.js
@@ -0,0 +1,22 @@
+var userName = prompt('Введите свое имя');
+console.log('Вас зовут: ' +  userName);
+
+var userAge = prompt('Введите свой возраст');
+console.log('Ваш возраст: ' + userAge);
+
+var pr1 = prompt('6+4=?')
+console.log('Ваш ответ: ' + pr1 + ' Правильный ответ: 10');
+
+var pr2 = prompt('12-2=?')
+console.log('Ваш ответ: ' + pr2 + ' Правильный ответ: 10');
+
+var pr3 = +prompt('2,5*4=?')
+console.log('Ваш ответ: ' + pr3 + ' Правильный ответ: 10');
+
+var pr4 = prompt('20/2=?')
+console.log('Ваш ответ: ' + pr4 + ' Правильный ответ: 10')
+
+var pr5 = prompt('3%3=?')
+console.log('Ваш ответ: ' + pr5 + ' Правильный ответ: 0')
+
+var console = prompt('Ввойдите в консоль для разработчика')
